20e4b566b5164542c14d50e639929f82188dbf18
t7m3 <toda7.mk3@gmail.com>
2020-09-29 15:23:35 +0900

    ボタンをタップすると、「ボタンがタップされました」と表示する処理を追加した。

--- app/src/main/java/com/example/helloandroid_20200928/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid_20200928/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.helloandroid_20200928
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapHere.setOnClickListener {
+            textView.text = "ボタンがタップされました。"
+        }
     }
 }
